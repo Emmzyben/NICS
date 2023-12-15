9a87b7cb9231fb7d09fed49b973ca4bddbb6838d
unknown <emmco96@gmail.com>
2023-12-15 18:17:16 +0100

    new

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+  var open = false;
+
+function openNav() {
+    var sideNav = document.getElementById("mySidenav");
+    
+    if (sideNav.style.width === "0px" || sideNav.style.width === "") {
+        sideNav.style.width = "250px";
+        open = true;
+    } else {
+        sideNav.style.width = "0";
+        open = false;
+    }
+}
+
+  
+ 
