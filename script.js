a8153c0ef6efd625156e5ab81d975e755e1cd1f2
unknown <emmco96@gmail.com>
2023-12-17 23:02:48 +0100

    new

--- script.js
+++ script.js
@@ -43,3 +43,25 @@ function toggleHeight3() {
       document.getElementById("inner").innerText="+";
     }
   }
+
+
+  const imageContainer = document.getElementById('imageContainer');
+
+  function slideImages() {
+    // Calculate the width of a single image (assuming all images have the same width)
+    const imageWidth = document.querySelector('.sliderImage').offsetWidth;
+
+    // Move the image container to the left by the width of one image
+    imageContainer.style.transform = `translateX(-${imageWidth}px)`;
+
+    // After a delay, move the first image to the end of the container
+    setTimeout(() => {
+      const firstImage = document.querySelector('.sliderImage');
+      imageContainer.appendChild(firstImage.cloneNode(true));
+      firstImage.remove();
+      imageContainer.style.transform = 'translateX(0)';
+    }, 500); // Adjust the delay based on the transition duration
+  }
+
+  // Call the slideImages function at regular intervals to create a sliding effect
+  setInterval(slideImages, 3000); 
